ba23311d496f97bb4589bad1ff75ca1ea1bd327d
Shivang Rastogi <shivangrastogi73@gmail.com>
2025-05-01 03:23:29 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import logo from "../assets/raviKumarLogo.webp";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img
+            src={logo}
+            className="mx-2"
+            width={50}
+            height={33}
+            alt="Logo"
+          />
+        </a>
+      </div>
+
+      <div className="mr-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/your-linkedin-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://www.github.com/in/your-github-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/in/your-instagram-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://twitter.com/your-twitter-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaSquareXTwitter />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
