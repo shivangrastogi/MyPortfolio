1bfb6b45bd69d1b8019b4f2db832c78247bd79b3
Shivang Rastogi <shivangrastogi73@gmail.com>
2025-05-01 05:34:24 +0530

    style changes again

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
           <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
         </div>
       </div>
-      <div className="container mx-auto px-8">
+      <div className="mx-auto max-w-[1440px] px-0">
         <Navbar />
         <Hero />
         <Technologies />
